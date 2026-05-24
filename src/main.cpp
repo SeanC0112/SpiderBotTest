a356e7db181812f99296bfa0f47c914c5c4764a4
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-24 01:46:48 -0700

    add in all of the servos

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,66 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// f:front, b:back, l:left, r:right, h:horizontal, v:vertical, i:inner, o:outer
 
-void setup() {
+int flh = 6;
+int flvi = 7;
+int flvo = 15;
+
+Servo flhServo;
+Servo flviServo;
+Servo flvoServo;
+
+int frh = 41;
+int frvi = 40;
+int frvo = 39;
+
+Servo frhServo;
+Servo frviServo;
+Servo frvoServo;
+
+int blh = 14;
+int blvi = 13;
+int blvo = 12;
+
+Servo blhServo;
+Servo blviServo;
+Servo blvoServo;
+
+int brh = 19;
+int brvi = 20;
+int brvo = 21;
+
+Servo brhServo;
+Servo brviServo;
+Servo brvoServo;
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+
+  flhServo.attach(flh, 500, 2400);
+  flviServo.attach(flvi, 500, 2400);
+  flvoServo.attach(flvo, 500, 2400);
+
+  frhServo.attach(frh, 500, 2400);
+  frviServo.attach(frvi, 500, 2400);
+  frvoServo.attach(frvo, 500, 2400);
+
+  blhServo.attach(blh, 500, 2400);
+  blviServo.attach(blvi, 500, 2400);
+  blvoServo.attach(blvo, 500, 2400);
+
+  brhServo.attach(brh, 500, 2400);
+  brviServo.attach(brvi, 500, 2400);
+  brvoServo.attach(brvo, 500, 2400);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
