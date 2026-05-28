185d5baa38eeba7f4978c49637681748d4aa45aa
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-27 23:40:02 -0700

    continue to define the needed structs and class

--- src/tripleJointedLeg.h
+++ src/tripleJointedLeg.h
@@ -2,6 +2,45 @@
 #define triplejointedleg_h
 #include <ESP32Servo.h>
 
+struct pose3D
+{
+    float x;
+    float y;
+    float z;
+
+    pose3D(float x, float y, float z) : x(x), y(y), z(z) {}
+};
+
+struct legState
+{
+    float hAngle;
+    float viAngle;
+    float voAngle;
+
+    legState(float hAngle, float viAngle, float voAngle) : hAngle(hAngle), viAngle(viAngle), voAngle(voAngle) {}
+};
+
+struct legConfig
+{
+    float innerLength;
+    float middleLength;
+    float outerLength;
+
+    int hPin;
+    int viPin;
+    int voPin;
+
+    float hOffset;
+    float viOffset;
+    float voOffset;
+
+    legConfig(float innerLength, float middleLength, float outerLength,
+              int hPin, int viPin, int voPin,
+              float hOffset = 0, float viOffset = 0, float voOffset = 0) : innerLength(innerLength), middleLength(middleLength), outerLength(outerLength),
+                                                                           hPin(hPin), viPin(viPin), voPin(voPin),
+                                                                           hOffset(hOffset), viOffset(viOffset), voOffset(voOffset) {}
+};
+
 class tripleJointedLeg
 {
 private:
@@ -9,9 +48,23 @@ private:
     Servo vi;
     Servo vo;
 
+    legConfig config;
+
+    pose3D currentPose;
+    pose3D targetPose;
+
+    legState currentState;
+    legState targetState;
+
 public:
-    tripleJointedLeg(/* args */);
+    tripleJointedLeg(legConfig config);
     ~tripleJointedLeg();
+
+    void moveToPose(pose3D targetPose);
+    void moveToState(legState targetState);
+
+    pose3D getCurrentPose();
+    legState getCurrentState();
 };
 
 #endif
